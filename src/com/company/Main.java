d727ceeae07f151e7ec78e7f63d58b8fe82c9806
lawliet1011 <ezreflex12@gmail.com>
2020-08-06 05:56:01 +0700

    Rua

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Nhap so nguyen duong :");
+        int n;
+        int a;
+        int b;
+        int chan;
+        int tong=0;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        while(n>0) {
+            a = n / 10;
+            b = n % 10;
+            if (b % 2 == 0) {
+                chan = b;
+                tong= chan +tong;
+            }
+            n=a;
+        }
+        System.out.println("Tong = " + tong);
+    }
+}
